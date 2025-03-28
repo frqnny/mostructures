e948be8f32cb1553dec54883e017f115c7ec3cca
frqnny <45723631+frqnny@users.noreply.github.com>
2025-03-28 17:15:07 -0400

    Update to 1.21.5

--- common/src/main/java/io/github/frqnny/mostructures/mixin/MixinLivingEntity.java
+++ common/src/main/java/io/github/frqnny/mostructures/mixin/MixinLivingEntity.java
@@ -20,7 +20,7 @@ public class MixinLivingEntity {
     public void dropItems(DamageSource source, CallbackInfo info) {
 
         if (((LivingEntity) (Object) this) instanceof RabbitEntity rabbit) {
-            if (rabbit.getVariant() == RabbitEntity.RabbitType.EVIL) {
+            if (rabbit.getVariant() == RabbitEntity.Variant.EVIL) {
                 ItemStack diamonds = new ItemStack(Items.DIAMOND);
                 diamonds.setCount(Math.max(8, rabbit.getRandom().nextInt(10)));
                 ItemUtils.spawnStack(rabbit.getEntityWorld(), rabbit.getX(), rabbit.getY() + 0.5, rabbit.getZ(), diamonds);
